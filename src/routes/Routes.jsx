a030a3f8a81b97bbe198c5d682a3ada37b9405bb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-09 20:30:01 +0600

    create about, contact and service routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,12 +3,34 @@ import MainLayout from "../layout/MainLayout"
 import LogIn from "../pages/LogIn/LogIn"
 import Register from "../pages/Register/Register"
 import Error from "../pages/Error/Error"
+import Home from "../pages/Home/Home"
+import About from "../pages/About/About"
+import Service from "../pages/Service/Service"
+import Contact from "../pages/Contact/Contact"
 
 const Routes = createBrowserRouter([
     {
         path:"/",
         element:<MainLayout/>,
         errorElement:<Error/>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>
+            },
+            {
+                path:"/about",
+                element:<About/>
+            },
+            {
+                path:"/services",
+                element:<Service/>
+            },
+            {
+                path:"/contact",
+                element:<Contact/>
+            }
+        ]
     },
     {
         path:"/logIn",
