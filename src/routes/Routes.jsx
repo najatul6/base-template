960e443e6c7b104250ddbe62e3ddb4e1311f0990
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-09 20:26:46 +0600

    create error page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom"
 import MainLayout from "../layout/MainLayout"
 import LogIn from "../pages/LogIn/LogIn"
 import Register from "../pages/Register/Register"
+import Error from "../pages/Error/Error"
 
 const Routes = createBrowserRouter([
     {
         path:"/",
         element:<MainLayout/>,
+        errorElement:<Error/>,
     },
     {
         path:"/logIn",
