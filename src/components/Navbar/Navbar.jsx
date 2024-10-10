96886ab28b5bd7d6e374d6f05d1941dba523b985
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-10 22:05:53 +0600

    update max width

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
       <header className="h-16 text-[15px] fixed inset-0 flex-center bg-[#18181A]">
-        <nav className="px-3.5 flex-center-between w-full max-w-7xl mx-auto">
+        <nav className="px-3.5 flex-center-between w-full max-w-[1920px] mx-auto">
           {/* Logo section  */}
           <div className="flex-center gap-x-3 z-[999] relative">
             <img src={Logo} alt="brand Logo" className="size-11" />
