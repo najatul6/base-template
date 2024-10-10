f9e8f3f48150a849d18d0a457c6f6dc69f637e64
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-10 23:19:42 +0600

    add submenu

--- src/components/Navbar/DesktopMenu.jsx
+++ src/components/Navbar/DesktopMenu.jsx
@@ -12,7 +12,28 @@ const DesktopMenu = ({ menu }) => {
         className="flex-center gap-1 px-3 py-1 cursor-pointer rounded-xl hover:bg-white/5"
       >
         {menu?.name}
-        {hasSubMenu && <ChevronDown className="mt-[0.6px] group-hover/link:rotate-180 duration-200" />}
+        {hasSubMenu && (
+          <ChevronDown className="mt-[0.6px] group-hover/link:rotate-180 duration-200" />
+        )}
+        {hasSubMenu && (
+          <div>
+            <div>
+              {menu?.subMenu?.map((subMenu, idx) => (
+                <div key={idx} className="">
+                  <div>
+                    <div>
+                      {subMenu?.icon && <subMenu.icon className="w-4 h-4" />}
+                    </div>
+                    <div>
+                      <h6>{subMenu?.name}</h6>
+                      <p>{subMenu?.desc}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </NavLink>
     </li>
   );
