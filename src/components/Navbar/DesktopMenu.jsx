803a7de7aa64c0ddbbaae9b5cfe53480f9db6247
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-10 21:45:19 +0600

    show conditionally icons

--- src/components/Navbar/DesktopMenu.jsx
+++ src/components/Navbar/DesktopMenu.jsx
@@ -1,16 +1,21 @@
 import PropTypes from "prop-types";
 import { ChevronDown } from "lucide-react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const DesktopMenu = ({ menu }) => {
   // Check if menu has sub-menu items
   const hasSubMenu = menu?.subMenu?.length > 0;
   return (
     <li>
-      <Link to={menu?.name}>{menu?.name}</Link>
-      {hasSubMenu && <ChevronDown className="mt-[0.6px]"/>}
+      <NavLink
+        to={menu?.name}
+        className="flex-center gap-1 px-3 py-1 cursor-pointer rounded-xl hover:bg-white/5"
+      >
+        {menu?.name}
+        {hasSubMenu && <ChevronDown className="mt-[0.6px]" />}
+      </NavLink>
     </li>
   );
-}
+};
 
 export default DesktopMenu;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,20 +3,24 @@ import { Menus } from "../../utils/utils";
 import DesktopMenu from "./DesktopMenu";
 const Navbar = () => {
   return (
-    <header className="h-16 text-[15px] fixed inset-0 flex-center bg-[#18181A]">
-      <nav className="px-3.5 flex-center-between w-full max-w-7xl mx-auto">
-        {/* Logo section  */}
-        <div className="flex-center gap-x-3 z-[999] relative">
-          <img src={Logo} alt="brand Logo" className="size-11" />
-          <h3 className="text-lg font-semibold">Template</h3>
-        </div>
-        {/* Menus section  */}
-        {/* Desktop section  */}
-        <ul>
-          {Menus.map(menu=><DesktopMenu menu={menu} key={menu?.name}/> )}
-        </ul>
-      </nav>
-    </header>
+    <div>
+      <header className="h-16 text-[15px] fixed inset-0 flex-center bg-[#18181A]">
+        <nav className="px-3.5 flex-center-between w-full max-w-7xl mx-auto">
+          {/* Logo section  */}
+          <div className="flex-center gap-x-3 z-[999] relative">
+            <img src={Logo} alt="brand Logo" className="size-11" />
+            <h3 className="text-lg font-semibold">Template</h3>
+          </div>
+          {/* Menus section  */}
+          {/* Desktop section  */}
+          <ul className="flex-center">
+            {Menus.map((menu) => (
+              <DesktopMenu menu={menu} key={menu?.name} />
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 };
 
