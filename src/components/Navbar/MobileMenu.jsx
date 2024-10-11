50aa2759f29bba751ac4f59a93195e69a02f091e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-11 21:43:22 +0600

    toggle sub menu item open

--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -1,38 +1,43 @@
-import { ChevronDown, Menu, X } from "lucide-react"
-import { useState } from "react"
+import { ChevronDown, Menu, X } from "lucide-react";
+import { useState } from "react";
 
-const MobileMenu = ({Menus}) => {
-    const [isOpen,setIsOpen]=useState(false)
-    const [clicked, setClicked]=useState(false)
-    const toggleDrawer=()=>{
-        setIsOpen(!isOpen)
-    }
+const MobileMenu = ({ Menus }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [clicked, setClicked] = useState(null);
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-        <button className="z-[999] relative" onClick={toggleDrawer}>
-            {
-                isOpen ? <X/>:<Menu/>
-            }
-        </button>
-        <div className="fixed left-0 right-0 top-16 overflow-y-auto h-full bg-[#18181A] backdrop-blur text-white p-6 ">
-            <ul>
-                {
-                    Menus?.map(({name,submenu},idx)=>{
-                        const hasSubMenu =submenu?.length>0
-                        return(
-                            <li key={name}>
-                                <span className="flex-center-between p-4 hover:bg-white/5 rounded-md cursor-pointer relative ">
-                                    {name}
-                                    {hasSubMenu && <ChevronDown/>}
-                                </span>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
+      <button className="z-[999] relative" onClick={toggleDrawer}>
+        {isOpen ? <X /> : <Menu />}
+      </button>
+      <div className="fixed left-0 right-0 top-16 overflow-y-auto h-full bg-[#18181A] backdrop-blur text-white p-6 ">
+        <ul>
+          {Menus?.map(({ name, submenu }, idx) => {
+            const hasSubMenu = submenu?.length > 0;
+            const isClicked = clicked === idx;
+            return (
+              <li key={name}>
+                <span
+                  onClick={() => setClicked(isClicked ? null : idx)}
+                  className="flex-center-between p-4 hover:bg-white/5 rounded-md cursor-pointer relative "
+                >
+                  {name}
+                  {hasSubMenu && (
+                    <ChevronDown
+                      className={`ml-auto ${isClicked && "rotate-180"}`}
+                    />
+                  )}
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MobileMenu
+export default MobileMenu;
