02a9d7524618940a2e95a6ed1be866f1ddea0011
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-11 21:31:37 +0600

    add some class for sub menus in mobile menu

--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -1,18 +1,36 @@
-import { Menu, X } from "lucide-react"
+import { ChevronDown, Menu, X } from "lucide-react"
 import { useState } from "react"
 
 const MobileMenu = ({Menus}) => {
     const [isOpen,setIsOpen]=useState(false)
+    const [clicked, setClicked]=useState(false)
     const toggleDrawer=()=>{
         setIsOpen(!isOpen)
     }
   return (
     <div>
-        <button onClick={toggleDrawer}>
+        <button className="z-[999] relative" onClick={toggleDrawer}>
             {
                 isOpen ? <X/>:<Menu/>
             }
         </button>
+        <div className="fixed left-0 right-0 top-16 overflow-y-auto h-full bg-[#18181A] backdrop-blur text-white p-6 ">
+            <ul>
+                {
+                    Menus?.map(({name,submenu},idx)=>{
+                        const hasSubMenu =submenu?.length>0
+                        return(
+                            <li key={name}>
+                                <span className="flex-center-between p-4 hover:bg-white/5 rounded-md cursor-pointer relative ">
+                                    {name}
+                                    {hasSubMenu && <ChevronDown/>}
+                                </span>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
     </div>
   )
 }
