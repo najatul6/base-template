22c3e0f0685e17b8a3367228b9f7738407ca781d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-11 23:29:24 +0600

    create some input field

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,40 @@
+import Select from "react-select"
+import { useForm, Controller } from "react-hook-form"
+import { Input } from "@material-ui/core"
 
 const Register = () => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: "",
+      select: {},
+    },
+  })
+  const onSubmit = (data) => console.log(data)
   return (
     <div>
-      <form>
-        <h1>Create an Account</h1>
-        
-      </form>
+     <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="firstName"
+        control={control}
+        render={({ field }) => <Input {...field} />}
+      />
+      <Controller
+        name="select"
+        control={control}
+        render={({ field }) => (
+          <Select
+            {...field}
+            options={[
+              { value: "chocolate", label: "Chocolate" },
+              { value: "strawberry", label: "Strawberry" },
+              { value: "vanilla", label: "Vanilla" },
+            ]}
+          />
+        )}
+      />
+      <input type="submit" />
+    </form>
+
     </div>
   )
 }
