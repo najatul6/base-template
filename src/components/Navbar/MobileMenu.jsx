18b11a1cf493707ed2875298a1ea417d5015ff90
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-11 21:51:18 +0600

    add framer motion for mobile menu

--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -1,5 +1,6 @@
 import { ChevronDown, Menu, X } from "lucide-react";
 import { useState } from "react";
+import {motion}from "framer-motion"
 
 const MobileMenu = ({ Menus }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,14 +14,20 @@ const MobileMenu = ({ Menus }) => {
       <button className="z-[999] relative" onClick={toggleDrawer}>
         {isOpen ? <X /> : <Menu />}
       </button>
-      <div className="fixed left-0 right-0 top-16 overflow-y-auto h-full bg-[#18181A] backdrop-blur text-white p-6 ">
+      <motion.div
+      initial={{x:"=100%"}}
+      animate={{x: isOpen? "0%" : "100%"}}
+       className="fixed left-0 right-0 top-16 overflow-y-auto h-full bg-[#18181A] backdrop-blur text-white p-6 ">
         <ul>
-          {Menus?.map(({ name, submenu }, idx) => {
-            const hasSubMenu = submenu?.length > 0;
+          {Menus?.map(({ name, subMenu }, idx) => {
+            // checking subMenu exits 
+            const hasSubMenu = subMenu?.length > 0;
+            // checking if clicked menu
             const isClicked = clicked === idx;
             return (
               <li key={name}>
                 <span
+                // toggle sub menu item open
                   onClick={() => setClicked(isClicked ? null : idx)}
                   className="flex-center-between p-4 hover:bg-white/5 rounded-md cursor-pointer relative "
                 >
@@ -31,11 +38,25 @@ const MobileMenu = ({ Menus }) => {
                     />
                   )}
                 </span>
+                {hasSubMenu &&(
+                    <motion.ul className="ml-5">
+                        {
+                            subMenu.map(({ name,icon:Icon }) => (
+                            <li key={name} className="p-2 flex-center hover:bg-white/5 rounded-md gap-x-2">
+                                <Icon size={17}/>
+                               <span>
+                               {name}
+                                </span> 
+                            </li>
+                            ))
+                        }
+                    </motion.ul>
+                )}
               </li>
             );
           })}
         </ul>
-      </div>
+      </motion.div>
     </div>
   );
 };
