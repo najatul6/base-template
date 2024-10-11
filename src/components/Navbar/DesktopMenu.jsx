9c426166b8fc174d9728cfcf763b7fa24e241f3a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-11 21:08:54 +0600

    add hover effect for  submenu

--- src/components/Navbar/DesktopMenu.jsx
+++ src/components/Navbar/DesktopMenu.jsx
@@ -1,12 +1,38 @@
 import PropTypes from "prop-types";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, Rotate3D } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const DesktopMenu = ({ menu }) => {
+  const [isHover,setIsHover]=useState(false);
+  const toggleHover=()=>{
+    setIsHover(!isHover);
+  }
+
+  // Animation Varients
+  const subMenuAnimate={
+    enter:{
+      opacity:1,
+      rotateX:0,
+      transition:{
+        duration:0.5,
+      },
+      display:"block",
+    },
+    exit:{
+      opacity:0,
+      rotateX:-15,
+      transition:{
+        duration:0.5,
+      },
+      display:"none",
+    }
+  }
   // Check if menu has sub-menu items
   const hasSubMenu = menu?.subMenu?.length > 0;
   return (
-    <li className="group/link">
+    <motion.li className="group/link">
       <NavLink
         to={`${menu?.name === "Home" ? "/" : menu?.name}`}
         className="flex-center gap-1 px-3 py-1 cursor-pointer rounded-xl hover:bg-white/5"
@@ -16,7 +42,7 @@ const DesktopMenu = ({ menu }) => {
           <ChevronDown className="mt-[0.6px] group-hover/link:rotate-180 duration-200" />
         )}
         {hasSubMenu && (
-          <div className="sub-menu">
+          <motion.div className="sub-menu" initial="exit" animate={isHover? "enter":"exit"} variants={subMenuAnimate}>
             <div
               className={`grid gap-7 ${
                 menu?.gridCols === 3
@@ -40,10 +66,10 @@ const DesktopMenu = ({ menu }) => {
                 </div>
               ))}
             </div>
-          </div>
+          </motion.div>
         )}
       </NavLink>
-    </li>
+    </motion.li>
   );
 };
 
