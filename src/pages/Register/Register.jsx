411b36a241a6896f629df5ee25f7ce4bb095f67f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-11 23:42:15 +0600

    get all data from this form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -7,8 +7,11 @@ const Register = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <label> Name</label>
       <input {...register("firstName", { required: true, maxLength: 20 })} />
+        <label> Name</label>
       <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+        <label> Name</label>
       <input type="number" {...register("age", { min: 18, max: 99 })} />
       <input type="submit" />
     </form>
