bc22fb52ebb4796518214588320ecd3597260aac
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-12 21:21:05 +0600

    import images

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,10 +1,43 @@
-
+import wave from "../../assets/wave.svg"
+import avatar from "../../assets/avatar.svg"
+import bg from "../../assets/"
 const LogIn = () => {
   
 
   return (
     <>
-    Log in
+    <img className="wave" src={wave}/>
+	<div className="container">
+		<div className="img">
+			<img src="img/bg.svg"/>
+		</div>
+		<div className="login-content">
+			<form>
+				<img src={avatar}/>
+				<h2 className="title">Welcome</h2>
+           		<div className="input-div one">
+           		   <div className="i">
+           		   		<i className="fas fa-user"></i>
+           		   </div>
+           		   <div className="div">
+           		   		<h5>Username</h5>
+           		   		<input type="text" className="input"/>
+           		   </div>
+           		</div>
+           		<div className="input-div pass">
+           		   <div className="i"> 
+           		    	<i className="fas fa-lock"></i>
+           		   </div>
+           		   <div className="div">
+           		    	<h5>Password</h5>
+           		    	<input type="password" className="input"/>
+            	   </div>
+            	</div>
+            	<a href="#">Forgot Password?</a>
+            	<input type="submit" className="btn" value="Login"/>
+            </form>
+        </div>
+    </div>
     </>
   );
 };
