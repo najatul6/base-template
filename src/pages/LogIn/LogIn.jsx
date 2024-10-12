ebb6bc094834656983b1d7c3bda585f2afabdcfe
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-12 23:45:44 +0600

    update image blur

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -5,7 +5,7 @@ import { FaUser } from "react-icons/fa";
 const LogIn = () => {
   return (
     <div className=" min-h-screen overflow-hidden relative">
-      <img className="fixed" src={wave} />
+      <img className="fixed bottom-0 -left-72 " src={wave} />
 
       {/* form  */}
       <div className="flex justify-center items-center min-h-screen">
@@ -14,35 +14,21 @@ const LogIn = () => {
           <div className="w-full">
             <img src={avatar} className="rounded-full w-[100px]" />
             <form className="space-y-6 font-[sans-serif] max-w-lg mx-auto">
-              <div className="relative flex items-center">
+              <div className="relative flex justify-center items-center">
                 <input
                   type="email"
-                  placeholder="Enter Email"
-                  className="px-2 py-3 bg-transparent text-black w-full text-sm border-b-2 focus:border-blue-500 outline-none"
+                  placeholder="Enter Username or Email Address"
+                  className="pl-12 px-5 py-3 bg-transparent font-semibold w-full  border-b-2 focus:border-[#1076FF] outline-none"
                 />
-                <FaUser className="w-[18px] h-[18px] absolute left-4"/>
-                
+                <FaUser className="w-[18px] h-[18px] absolute  left-4"/>
               </div>
-
-              {/* password  */}
-              <div className="relative flex items-center">
+              <div className="relative flex justify-center items-center">
                 <input
                   type="password"
-                  placeholder="Enter Password"
-                  className="px-2 py-3 bg-white text-black w-full text-sm border-b-2 focus:border-blue-500 outline-none"
+                  placeholder="Enter Your Password"
+                  className="pl-12 px-5 py-3 bg-transparent font-semibold w-full  border-b-2 focus:border-[#1076FF] outline-none"
                 />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#bbb"
-                  stroke="#bbb"
-                  className="w-[18px] h-[18px] absolute right-4 cursor-pointer"
-                  viewBox="0 0 128 128"
-                >
-                  <path
-                    d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                    data-original="#000000"
-                  ></path>
-                </svg>
+                <FaUser className="w-[18px] h-[18px] absolute  left-4"/>
               </div>
 
               <div className="flex">
