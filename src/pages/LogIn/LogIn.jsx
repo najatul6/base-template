7f9bddc7a31388f81ea04cc9ea4da7e67981d1fa
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-13 03:47:25 +0600

    add some wave for log in page

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,23 +1,25 @@
 import wave from "../../assets/wave.svg";
 import avatar from "../../assets/avatar.svg";
-// import bg from "../../assets/userImg.svg";
 import { FaUser } from "react-icons/fa";
 import { useState } from "react";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { IoEye, IoEyeOff } from "react-icons/io5";
+import { Link } from "react-router-dom";
 const LogIn = () => {
   const [isEyeOpen, setIsEyeOpen] = useState(false);
   return (
-    <div className=" min-h-screen overflow-hidden relative">
-      <img className="fixed bottom-0 lg:-left-72 " src={wave} />
+    <div className=" min-h-screen overflow-hidden relative bg-red-400">
+      <img className="fixed bottom-0 " src={wave} />
 
       {/* form  */}
       <div className="flex justify-center items-center min-h-screen">
-        {/* <img src={bg} className="w-full" /> */}
-        <div className=" flex justify-center items-center  gap-6 w-11/12 lg:w-9/12 lg:h-[640px] bg-white/10 rounded-xl backdrop-blur-lg shadow-xl px-4 py-10">
+        <div className=" flex justify-center items-center  gap-6 w-11/12 lg:w-8/12 lg:h-[540px] bg-white/10 rounded-xl backdrop-blur-lg shadow-xl px-4 py-10">
           <div className="w-full">
-            <img src={avatar} className="rounded-full w-[100px]" />
+            <div className="flex-center-item-row">
+              <img src={avatar} className="rounded-full w-[100px]" />
+            </div>
             <form className="space-y-6 font-[sans-serif] max-w-lg mx-auto">
+            <h1 className="text-3xl uppercase font-bold tracking-[0.25rem] text-center py-5">Welcome</h1>
               <div className="relative flex justify-center items-center">
                 <input
                   type="email"
@@ -28,7 +30,7 @@ const LogIn = () => {
               </div>
               <div className="relative flex justify-center items-center">
                 <input
-                  type={`${isEyeOpen?"password":"text"}`}
+                  type={`${isEyeOpen ? "password" : "text"}`}
                   placeholder="Enter Your Password"
                   className="pl-12 px-5 py-3 bg-transparent font-semibold w-full  border-b-2 focus:border-[#1076FF] outline-none"
                 />
@@ -42,14 +44,13 @@ const LogIn = () => {
                 </div>
               </div>
 
-              <div className="flex">
-                <input type="checkbox" className="w-4" />
-                <label className="text-sm ml-4 ">Remember me</label>
+              <div className="flex justify-end">
+                <Link>Forget Password?</Link>
               </div>
 
               <button
                 type="submit"
-                className="!mt-8 px-8 py-2.5 bg-blue-500 text-sm text-white hover:bg-blue-600 rounded-sm"
+                className="!mt-8 px-8 py-2.5 bg-blue-500 text-sm text-white hover:bg-blue-600 rounded-full w-full"
               >
                 Submit
               </button>
