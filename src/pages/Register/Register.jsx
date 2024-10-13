4403190f6247f78b4a4ac6a2c74094577ab14707
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-13 15:46:28 +0600

    also add email field in register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { IoEye, IoEyeOff } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { CgMail } from "react-icons/cg";
 const Register = () => {
   const [isEyeOpen, setIsEyeOpen] = useState(false);
   return (
@@ -40,7 +41,7 @@ const Register = () => {
                   placeholder="Enter Username or Email Address"
                   className="pl-12 px-5 text-white placeholder-white py-3 bg-transparent font-semibold w-full  border-b-2 focus:border-[#1076FF] outline-none"
                 />
-                <FaUser className="w-[18px] h-[18px] absolute  left-4" />
+                <CgMail  className="w-[18px] h-[18px] absolute  left-4" />
               </div>
               <div className="relative flex justify-center items-center">
                 <input
