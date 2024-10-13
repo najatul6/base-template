a557fb752327b582033a9c6e1bef2726ec2c5629
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-13 16:16:54 +0600

    add icon for home redirect

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,6 +1,7 @@
 import Lottie from "lottie-react";
 import { Link, useRouteError } from "react-router-dom";
 import errorImg from "../../assets/Error.json"
+import { Home } from "lucide-react";
 
 const Error = () => {
   const error = useRouteError();
@@ -9,7 +10,8 @@ const Error = () => {
     <div className="bg-[#181181A]">
       <div className="flex flex-col min-h-screen items-center justify-center">
         <Lottie animationData={errorImg}></Lottie>
-        <Link className="bg-white/5 py-4 px-5 uppercase font-bold rounded-full">
+        <Link className="bg-white/5 py-4 px-5 uppercase font-bold rounded-full flex justify-center items-center gap-4 hover:bg-white/10">
+        <Home/>
         Go to home
         </Link>
       </div>
