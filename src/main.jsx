efdcf39d32ead59e0e90f7f7583f612207a89ae0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-21 20:05:22 +0600

    wrap with auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Routes from "./routes/Routes";
 import "./index.css";
+import { AuthContext } from "./providers/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={Routes} />
+    <AuthContext>
+      <RouterProvider router={Routes} />
+    </AuthContext>
   </StrictMode>
 );
