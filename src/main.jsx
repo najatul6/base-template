da0f211504c319fd910260122aa6554b31c3bf77
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-21 22:36:24 +0600

    Add react toastify

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import Routes from "./routes/Routes";
 import "./index.css";
 import { AuthContext } from "./providers/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContext>
+    <ToastContainer position="top-center" autoClose={3000} />
       <RouterProvider router={Routes} />
     </AuthContext>
   </StrictMode>
