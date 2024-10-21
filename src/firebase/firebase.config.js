9d498e3ded92b048051ab0854e0ab332b73b5be4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-21 23:10:46 +0600

    solve problem and fix file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.FIREBASE_CONFIG_APIKEY,
-  authDomain:import.meta.env.FIREBASE_CONFIG_AUTHDOMAIN,
-  projectId:import.meta.env.FIREBASE_CONFIG_PROJECTID,
-  storageBucket:import.meta.env.FIREBASE_CONFIG_STORAGEBUCKET,
-  messagingSenderId:import.meta.env.FIREBASE_CONFIG_MESSAGINGSENDERID,
-  appId:import.meta.env.FIREBASE_CONFIG_APPID,
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain:import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket:import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_FIREBASE_APPID,
 };
 
 // Initialize Firebase
