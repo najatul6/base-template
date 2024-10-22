fe1abab8aac16658a80f22cd3552c0e846d840b4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-22 23:04:03 +0600

    update routes links

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,5 +3,4 @@ export const routes = [
   { id: 2, path: "/about", name: "About", component: "About" },
   { id: 3, path: "/contact", name: "Contact", component: "Contact" },
   { id: 4, path: "/services", name: "Services", component: "Services" },
-  { id: 5, path: "/profile", name: "Profile", component: "Profile" },
 ];
